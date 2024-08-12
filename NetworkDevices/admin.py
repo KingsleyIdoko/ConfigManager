61e0d9ab1836e66e18d5d0db6b6efc84f7177abd
kingsley <idoko.omokhodion@gmail.com>
2024-08-12 23:02:31 +0200

    updating networkdevicemodels

--- NetworkDevices/admin.py
+++ NetworkDevices/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from NetworkDevices.models import NetworkDevice
 
 # Register your models here.
+
+class NetworkDeviceAdmin(admin.ModelAdmin):
+    list_display = ['name','vendor_type','ip_address']
+    
+
+admin.site.register(NetworkDevice, NetworkDeviceAdmin)
